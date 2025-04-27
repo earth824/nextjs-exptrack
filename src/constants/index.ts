bbd3476d84ab02ad24981c0575be1005004c1075
Pisut Theeraphanphichate <pisut.flyinggiraffe@gmail.com>
2025-04-27 14:51:56 +0700

    Removed unused and setup shadcn

---
+++ src/constants/index.ts
@@ -0,0 +1,3 @@
+export const APP_NAME = process.env.NEXT_PUBLIC_APP_NAME || 'Expense Tracker';
+export const APP_DESCRIPTION =
+  process.env.NEXT_PUBLIC_APP_DESCRIPTION || 'Basic simple expense tracker web application using Next.js';
